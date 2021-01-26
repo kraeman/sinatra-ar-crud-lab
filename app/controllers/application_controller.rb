ce6828396c2e32fe419da02dea10dbe0b366a03f
krae <kraewind@gmail.com>
2021-01-26 11:28:16 -0500

    commit tree 293297726c1b67b72024ebddd3e02c8befd7a56d

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,52 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @id = params[:id]
+
+    erb :show
+  end
+
+
+  post '/articles' do
+    @new = Article.find_or_create_by(title: params[:articles][:title], content: params[:articles][:content])
+    redirect "/articles/#{@new.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @id = params[:id]
+    @article = Article.find(@id)
+    @title = @article.title
+    @content = @article.content
+
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+
+    @article2 = Article.all.last
+    @article2.title = params[:articles][:title]
+    @article2.content = params[:articles][:content]
+    @article2.save
+  
+
+    redirect "/articles/#{@article2.id}"
+    
+  end
+
+  delete "/articles/:id" do
+    @article3 = Article.all.last.delete
+    redirect "/articles"
+  end
 end
